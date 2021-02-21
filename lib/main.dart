d7b04376a883506254fffd994158909b01d6cfd4
xkeyC <3334969096@qq.com>
2021-02-22 04:05:44 +0800

    remove go; add Platform;

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,17 @@
+import 'dart:io' show Platform;
+
+import 'package:fluam_app/conf.dart';
 import 'package:fluam_app/ui/SplashUI.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-void main() {
-  SystemChrome.setSystemUIOverlayStyle(
-      SystemUiOverlayStyle(statusBarColor: Colors.transparent));
+void main() async {
+  if (Platform.isAndroid) {
+    SystemChrome.setSystemUIOverlayStyle(
+        SystemUiOverlayStyle(statusBarColor: Colors.transparent));
+  } else if (Platform.isWindows || Platform.isMacOS || Platform.isLinux) {
+    AppConf.setDesktop();
+  }
+
   runApp(SplashUI());
 }
